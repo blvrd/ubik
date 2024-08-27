5bc669bdf2e01220b9b7f1694e209a45331db88d
Garrett Martin <garrett@blvrd.co>
2024-08-27 15:08:37 -0700

    Trying to get the check example script to sleep properly

--- check.sh
+++ check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+echo "hiiiiiiiiiiiiiiii"
+sleep 5
 # Use $RANDOM to generate a random number and % 2 to get either 0 or 1
 random_number=$((RANDOM % 2))
 
-exit $random_number
+exit random_number
