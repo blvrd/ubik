f25a3c1ba314ae78efc0b5966ab01705591b5d48
Garrett Martin <garrett@blvrd.co>
2024-08-28 14:46:39 -0700

    Just exit with error

--- check.sh
+++ check.sh
@@ -4,7 +4,5 @@ set -euxo pipefail
 
 echo "hiiiiiiiiiiiiiiii"
 sleep 5
-# Use $RANDOM to generate a random number and % 2 to get either 0 or 1
-random_number=$((RANDOM % 2))
 
-exit random_number
+exit 1
